10343880314cb7f2e8e898b65691b43b04dcd3fe
hongsikcho <jjdd9758@naver.com>
2022-08-08 11:36:22 +0900

    메인컨트롤러

---
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -0,0 +1,13 @@
+package com.ll.exam.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/ssb")
+    public void index(){
+        System.out.println("12314");
+    }
+}
