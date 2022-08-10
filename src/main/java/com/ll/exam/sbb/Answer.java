249d175dfe19fe6a415baabc63164aeb942f917a
hongsikcho <jjdd9758@naver.com>
2022-08-10 14:25:52 +0900

    answer question 다시 생성

--- src/main/java/com/ll/exam/sbb/Answer.java
+++ src/main/java/com/ll/exam/sbb/Answer.java
@@ -1,17 +1,10 @@
 package com.ll.exam.sbb;
 
-import java.time.LocalDateTime;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-
-
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
