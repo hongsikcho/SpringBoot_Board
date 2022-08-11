22aadd4cc6479d76f56d9d4659d35607833e5229
hongsikcho <jjdd9758@naver.com>
2022-08-11 13:43:19 +0900

    jpa 리포지터리 연습

---
+++ src/main/java/com/ll/exam/sbb/QuestionRepository.java
@@ -0,0 +1,20 @@
+package com.ll.exam.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+    Question findBySubject(String s);
+
+    Question findBySubjectAndContent(String s, String s1);
+
+    @Query(value = "SET FOREIGN_KEY_CHECKS = 0;" ,nativeQuery = true)
+    Question truncate1();
+
+    @Query(value = "truncate Question;" ,nativeQuery = true)
+    Question truncate2();
+
+    @Query(value = "SET FOREIGN_KEY_CHECKS = 1;" ,nativeQuery = true)
+    Question truncate3();
+}
